838ce9403b6a0a7a41ef123d7741b8c54607ab7b
gil468 <gil123991gmail.com>
2023-11-03 13:47:03 +0200

    lesson 37

--- controllers/todocontroller.js
+++ controllers/todocontroller.js
@@ -10,38 +10,47 @@ var todoSchema = new mongoose.Schema({
 });
 
 var Todo = mongoose.model('Todo', todoSchema);
-var itemOne = new Todo({item: 'get flowers'});
-itemOne.save()
-  .then(() => {
-    console.log('Item saved');
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+// var itemOne = new Todo({item: 'get flowers'});
+// itemOne.save()
+//   .then(() => {
+//     console.log('Item saved');
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
 
-let data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
+//let data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
+let data = {};
 let urlencodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function(app){
     
 app.get('/todo', (req, res) => {
+    //get data from mongodb and pass it to view
+    Todo.find()
+    .then(() => {
+        res.render('todo', {todos: data});
+      })
+      .catch((err) => {
+        console.error(err);
+      });
     res.render(__dirname + '/views/todo', {todos: data});
 });
 
 app.post('/todo', urlencodedParser, (req, res) => {
-    console.log(urlencodedParser);
-    data.push(req.body);
-    console.log(req.body);
-    console.log(data);
-    res.json(data);
-    console.log(res);
+    // get data from the view and add it to mongodb
+    var newTodo = Todo(req.body).save(function(err,data){
+        if(err) throw err;
+        res.json(data);
+    });
 });
 
 app.delete('/todo', (req, res) => {
-    data = data.filter((todo) => {
-        return todo.item.replace(/ /g, '-') !== req.params.item;
-    });
-    res.json(data);
+    //Delete the requested item from mongodb
+    Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err,data){
+        if (err) throw err;
+        res.json(data);
+    })
 });
 
 };
